0bb6913343a55ebdaa5fc6a5b6b6eccf0c750905
Ogievych Oleksandr Mykolayovych <ohiievych_ak24@nuwm.edu.ua>
2025-10-07 14:45:21 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,24 +1,69 @@
 ﻿using System;
 
-namespace LabWork
+namespace Lab3
 {
-    // Даний проект є шаблоном для виконання лабораторних робіт
-    // з курсу "Об'єктно-орієнтоване програмування та патерни проектування"
-    // Необхідно змінювати і дописувати код лише в цьому проекті
-    // Відео-інструкції щодо роботи з github можна переглянути 
-    // за посиланням https://www.youtube.com/@ViktorZhukovskyy/videos 
+    public class GeometricProgression
+    {
+        private double a0, q;
+        private int n;
 
-    class Result
-    { 
-    // TODO: do it !
+        public GeometricProgression(double a0, double q, int n)
+        {
+            this.a0 = a0;
+            this.q = q;
+            this.n = n;
+        }
+
+        public double LastTerm()
+        {
+            return a0 * Math.Pow(q, n - 1);
+        }
+
+        ~GeometricProgression()
+        {
+            Console.WriteLine("Об'єкт знищено");
+        }
     }
-    
+
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            
-            Console.WriteLine("Hello World!");
+            Console.Write("Кількість прогресій: ");
+            int m = int.Parse(Console.ReadLine());
+            GeometricProgression[] gp = new GeometricProgression[m];
+
+            for (int i = 0; i < m; i++)
+            {
+                Console.WriteLine($"\nПрогресія #{i + 1}");
+                Console.Write("a0 = ");
+                double a0 = double.Parse(Console.ReadLine());
+                Console.Write("q = ");
+                double q = double.Parse(Console.ReadLine());
+                Console.Write("n = ");
+                int n = int.Parse(Console.ReadLine());
+                gp[i] = new GeometricProgression(a0, q, n);
+            }
+
+            int maxIndex = 0;
+            double maxValue = gp[0].LastTerm();
+
+            for (int i = 1; i < m; i++)
+            {
+                double value = gp[i].LastTerm();
+                if (value > maxValue)
+                {
+                    maxValue = value;
+                    maxIndex = i;
+                }
+            }
+
+            Console.WriteLine($"\nНайбільший останній член у прогресії #{maxIndex + 1}: {maxValue}");
+
+            gp = null;
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
         }
     }
 }
+
